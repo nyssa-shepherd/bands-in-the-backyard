e8ce4198f6910b4b436b0472decdbe48733211c1
Nyssa Keller <nyssa1@gmail.com>
2018-02-15 14:42:29 -0700

    Add setLocation action and reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { eventReducer } from './eventReducer.js';
+import { locationReducer } from './locationReducer.js';
 
 const rootReducer = combineReducers({
-    events: eventReducer
+  events: eventReducer,
+  location: locationReducer
 });
 
 export default rootReducer;
