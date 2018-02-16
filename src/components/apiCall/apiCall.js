a260fdfa90f499db8758f182722efb9dc804cfdb
Nyssa Keller <nyssa1@gmail.com>
2018-02-16 13:03:15 -0700

    Clean api data returned from fetch

--- src/components/apiCall/apiCall.js
+++ src/components/apiCall/apiCall.js
@@ -6,10 +6,23 @@ const fetchApi = async(locationKey) => {
       `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=${locationKey}&apikey=${apiKey}`
     );
     const data = await initialFetch.json();
-    return data._embedded.events;
+    return cleanData(data._embedded.events);
   } catch (error) {
     throw Error;
   }
+};
+
+const cleanData = eventArray => {
+  const cleanData =  eventArray.map(async event => {
+    return await {
+      name: event.name,
+      city: event._embedded.venues[0].city.name,
+      state: event._embedded.venues[0].state.name,
+      venue: event._embedded.venues[0].name,
+      ticketUrl: event._embedded.venues[0].url
+    }
+  });
+  return Promise.all(cleanData);
 };
 
 export default fetchApi;
