9c86358275826305280f745b7b3fdda2332a9f6d
Nyssa Keller <nyssa1@gmail.com>
2018-02-16 12:35:44 -0700

    Refactor App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addEvents, fetchApiData } from '../../actions/actions.js';
-import fetchApi from '../apiCall/apiCall.js';
 import Header from '../Header/Header.js';
-import withRouter from 'react-router-dom/withRouter';
 
-class App extends Component {
-
-  componentDidMount = async() => {
-    //await this.props.fetchApiData();
-  }
-  
+class App extends Component {  
   render() {
     return (
       <div className="App">
@@ -20,15 +11,6 @@ class App extends Component {
   }
 }
 
-export const mapStateToProps = store => ({
-  events: store.events
-});
-
-export const mapDispatchToProps = dispatch => ({
-  //addEvents: event => dispatch(addEvents(event)),
-  fetchApiData: () => dispatch(fetchApiData())
-});
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default App;
 
 
