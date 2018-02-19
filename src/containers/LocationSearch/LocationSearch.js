59dfbd01b268bdcf4e92457d3542eddd9ecc7d06
Nyssa Keller <nyssa1@gmail.com>
2018-02-18 20:28:32 -0700

    Edit location object

--- src/containers/LocationSearch/LocationSearch.js
+++ src/containers/LocationSearch/LocationSearch.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { setLocation, fetchApiEvents } from '../../actions/actions.js';
 import { connect } from 'react-redux';
+import locationObj from '../../locationObject.js';
 
 
 class LocationSearch extends Component {
@@ -20,6 +21,7 @@ class LocationSearch extends Component {
 
   submitHandler = async(e) => {
     e.preventDefault();
+    console.log(locationObj[this.state.location]);
     await this.props.fetchApiEvents(this.props.location);
   }
 
