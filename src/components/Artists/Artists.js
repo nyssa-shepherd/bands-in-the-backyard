b8a8469c9a9c563c1efe04f7d84be43193b8eb63
Nyssa Keller <nyssa1@gmail.com>
2018-02-21 12:08:38 -0700

    Refactor set favoriteArtists out of search, put it in Artists.js

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Search from '../../containers/Search/Search.js';
 import CardContainer from '../../containers/CardContainer/CardContainer.js';
+import { 
+  setArtistInLocation } from '../../actions/actions.js';
 
-const Artists = () => {
-  return (
-    <div>
-      <Search />
-      <CardContainer />
-    </div>
-  );
-};
+export class Artists extends Component {
+  setFavoriteArtists = async() => {
+    const { location, allArtistEvents, setArtistInLocation } = this.props;
+    const splitLocation = location.split(', ');
 
-export default Artists;
+    let matchLocation = await allArtistEvents.filter(artist => {
+      return artist.state === splitLocation[1];
+    });
+ 
+    setArtistInLocation(matchLocation);
+  }
+
+  render () {  
+    return (
+      <div>
+        <Search setFavoriteArtists={this.setFavoriteArtists}/>
+        <CardContainer />
+      </div>
+    );
+  }
+}
+
+export const mapStateToProps = store => ({
+  location: store.location,
+  allArtistEvents: store.allArtistEvents
+});
+
+export const mapDispatchToProps = dispatch => ({
+  setArtistInLocation: artist => dispatch(setArtistInLocation(artist))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Artists);
