bc2ecc7caabe22a195bf9208f512d72cddba183f
Nyssa Keller <nyssa1@gmail.com>
2018-02-21 18:17:30 -0700

    Test mapStateToProps in Artists.js

--- src/containers/Artists/Artists.test.js
+++ src/containers/Artists/Artists.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Artists from './Artists.js';
+import { Artists, mapStateToProps } from './Artists.js';
 
 describe('Artists', () => {
 
@@ -9,4 +9,21 @@ describe('Artists', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('map the store correctly', () => {
+    const artist = {
+      key: 9,
+      name: 'name',
+      city: 'city',
+      state: 'state',
+      date: 'date',
+      time: 'time',
+      venue: 'venue',
+      ticketUrl: 'url'
+    };
+    const mockStore = {location: 'denver, co', allArtistEvents: [artist]};
+    const mapped = mapStateToProps(mockStore);
+
+    expect(mapped.location).toEqual(mockStore.location);
+    expect(mapped.allArtistEvents).toEqual(mockStore.allArtistEvents);
+  });
 });
